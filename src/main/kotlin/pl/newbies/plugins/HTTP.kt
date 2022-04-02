47129be30fe3c92c9bde9c2af59ba9f01b025164
JUtupe <petryszyn@o2.pl>
2022-04-02 21:45:20 +0200

    Auth header CORS hotfix

--- src/main/kotlin/pl/newbies/plugins/HTTP.kt
+++ src/main/kotlin/pl/newbies/plugins/HTTP.kt
@@ -1,5 +1,6 @@
 package pl.newbies.plugins
 
+import io.ktor.http.HttpHeaders
 import io.ktor.http.HttpMethod
 import io.ktor.server.application.Application
 import io.ktor.server.application.install
@@ -8,11 +9,14 @@ import io.ktor.server.plugins.CORS
 fun Application.configureHTTP() {
     install(CORS) {
         anyHost()
-        host("10.10.10.10:8080")
         allowNonSimpleContentTypes = true
+
         method(HttpMethod.Options)
         method(HttpMethod.Put)
         method(HttpMethod.Patch)
         method(HttpMethod.Delete)
+
+        header(HttpHeaders.Authorization)
+        header(HttpHeaders.ContentLength)
     }
 }
