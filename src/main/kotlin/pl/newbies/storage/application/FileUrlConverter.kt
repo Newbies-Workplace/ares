d352b76fd6107a5d7da3f363ce7f2bff8bf43b93
JUtupe <petryszyn@o2.pl>
2022-04-11 21:16:07 +0200

    cleanup

--- src/main/kotlin/pl/newbies/storage/application/FileUrlConverter.kt
+++ src/main/kotlin/pl/newbies/storage/application/FileUrlConverter.kt
@@ -4,14 +4,14 @@ import pl.newbies.storage.application.model.FileUrlResponse
 import pl.newbies.storage.domain.model.FileResource
 
 class FileUrlConverter(
-    private val fileServiceUrl: String
+    private val storageServiceUrl: String
 ) {
 
     fun convert(file: FileResource): FileUrlResponse =
         convert(file.pathWithName)
 
     fun convert(pathWithName: String): FileUrlResponse {
-        val fullFileUrl = "$fileServiceUrl${StorageController.DOWNLOAD_FILE_V1_PATH}$pathWithName"
+        val fullFileUrl = "$storageServiceUrl${StorageController.DOWNLOAD_FILE_V1_PATH}$pathWithName"
 
         return FileUrlResponse(fullFileUrl)
     }
