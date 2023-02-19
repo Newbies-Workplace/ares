28740bfaec7788208862b121a08119cd826c5b07
JUtupe <petryszyn@o2.pl>
2023-02-19 13:59:53 +0100

    tests

--- src/test/kotlin/pl/newbies/util/GraphQLTestActions.kt
+++ src/test/kotlin/pl/newbies/util/GraphQLTestActions.kt
@@ -2,12 +2,17 @@ package pl.newbies.util
 
 import io.ktor.client.request.bearerAuth
 import io.ktor.server.testing.ApplicationTestBuilder
+import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.Assertions.assertTrue
 import pl.newbies.auth.application.model.AuthResponse
+import pl.newbies.generated.CreateLectureInviteMutation
 import pl.newbies.generated.FollowEventMutation
 import pl.newbies.generated.RateLectureMutation
 import pl.newbies.generated.UnfollowEventMutation
+import pl.newbies.generated.createlectureinvitemutation.LectureInviteResponse
+import pl.newbies.generated.inputs.LectureInviteRequestInput
 import pl.newbies.generated.inputs.LectureRateRequestInput
+import pl.newbies.lecture.application.model.LectureInviteRequest
 import pl.newbies.lecture.application.model.LectureRateRequest
 
 suspend fun ApplicationTestBuilder.rateLecture(
@@ -29,6 +34,29 @@ suspend fun ApplicationTestBuilder.rateLecture(
     ) {
         bearerAuth(authResponse.accessToken)
     }
+}
+
+suspend fun ApplicationTestBuilder.createLectureInvite(
+    authResponse: AuthResponse,
+    lectureId: String,
+    request: LectureInviteRequest = TestData.createLectureInviteRequest(),
+): LectureInviteResponse {
+    val res = graphQLClient.execute(
+        CreateLectureInviteMutation(
+            CreateLectureInviteMutation.Variables(
+                lectureId = lectureId,
+                request = LectureInviteRequestInput(
+                    name = request.name,
+                ),
+            )
+        )
+    ) {
+        bearerAuth(authResponse.accessToken)
+    }
+
+    assertNotNull(res.data?.createLectureInvite)
+
+    return res.data?.createLectureInvite!!
 }
 
 suspend fun ApplicationTestBuilder.followEvent(authResponse: AuthResponse, eventId: String) {
